2fca8ae1af66915d343013f3d7027b2d69e84f6a
italoesilva <italoliveira95@hotmail.com>
2026-05-13 15:21:34 -0300

    car-rental-java

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,12 @@
 package application;
 
+import application.entities.CarRental;
+import application.entities.Vehicle;
 import model.services.BrazilTaxService;
+import model.services.RentalService;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -11,22 +16,32 @@ public class Program {
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-/*
+
         DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 
         System.out.println("Entre com os dados do aluguel: ");
         System.out.print("Modelo do carro: ");
         String carModel = sc.nextLine();
-        System.out.println("Retirada (dd/MM/yyyy HH:mm): ");
+        System.out.print("Retirada (dd/MM/yyyy HH:mm): ");
         LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
-        System.out.println("Retorno (dd/MM/yyyy HH:mm): ");
+        System.out.print("Retorno (dd/MM/yyyy HH:mm): ");
         LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
 
         CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
-*/
-        BrazilTaxService taxService = new BrazilTaxService();
 
-        System.out.println(taxService.tax(390.0));
+        System.out.print("Entre com preço por hora: ");
+        double pricePerHour = sc.nextDouble();
+        System.out.print("Entre com o preço por dia: ");
+        double pricePerDay = sc.nextDouble();
+
+        RentalService rentalService = new RentalService(pricePerHour, pricePerDay, new BrazilTaxService());
+
+        rentalService.processInvoice(cr);
+
+        System.out.println("FATURA: ");
+        System.out.println("Pagamento básico " + String.format("%.2f", cr.getInvoice().getBasicPayment()));
+        System.out.println("Imposto: " + String.format("%.2f", cr.getInvoice().getTax()));
+        System.out.println("Pagamento total: " + String.format("%.2f", cr.getInvoice().getTotalPayment()));
 
         sc.close();
     }
--- src/model/services/RentalService.java
+++ src/model/services/RentalService.java
@@ -1,12 +1,38 @@
 package model.services;
 
+import application.entities.CarRental;
+import application.entities.Invoice;
+
+import java.time.Duration;
+
 public class RentalService {
 
     private Double pricePerHour;
     private Double pricePerDay;
 
-    private BrazilTaxService taxService;
+    private TaxService taxService;
 
 
-    
+    public RentalService(Double pricePerHour, Double pricePerDay, TaxService taxService) {
+        this.pricePerHour = pricePerHour;
+        this.pricePerDay = pricePerDay;
+        this.taxService = taxService;
+    }
+
+    public void processInvoice(CarRental carRental) {
+
+        double minutes = Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
+        double hours = minutes / 60.0;
+
+        double basicPayment;
+        if (hours <= 12.0) {
+            basicPayment = pricePerHour * Math.ceil(hours);
+        } else {
+            basicPayment =  pricePerDay * Math.ceil(hours / 24.00);
+        }
+        double tax = taxService.tax(basicPayment);
+
+
+        carRental.setInvoice(new Invoice(basicPayment, tax));
+    }
 }
